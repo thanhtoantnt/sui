d283c099876f567ae20d0603fd7b1b0629cd53f5
thanhtoantnt <thanhtoantnt@gmail.com>
2023-09-19 18:31:16 +0800

    make the crate public

--- external-crates/move/move-vm/runtime/src/loader.rs
+++ external-crates/move/move-vm/runtime/src/loader.rs
@@ -1420,7 +1420,7 @@ enum BinaryType {
 // A Resolver is a simple and small structure allocated on the stack and used by the
 // interpreter. It's the only API known to the interpreter and it's tailored to the interpreter
 // needs.
-pub(crate) struct Resolver<'a> {
+pub struct Resolver<'a> {
     loader: &'a Loader,
     binary: BinaryType,
 }
